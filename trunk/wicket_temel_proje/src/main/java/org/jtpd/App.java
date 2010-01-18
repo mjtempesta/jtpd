7c130ef1391baaad65116538a8940cb90ede8050
altuga <altuga@295b7934-6740-0410-a4b5-11775588ede9>
2010-01-18 12:54:11 +0000

    Wicket + maven

---
+++ trunk/wicket_temel_proje/src/main/java/org/jtpd/App.java
@@ -0,0 +1,13 @@
+package org.jtpd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
